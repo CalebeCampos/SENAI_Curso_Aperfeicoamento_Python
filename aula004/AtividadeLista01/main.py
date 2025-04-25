397784c5ad0ecaaee9122648d00a4d5305ec0315
CalebeCampos <calebe_josue@hotmail.com>
2025-04-25 19:52:22 -0300

    atualizacao dos arquivos da atividadeLista01

--- aula004/AtividadeLista01/main.py
+++ aula004/AtividadeLista01/main.py
@@ -1,21 +1,29 @@
-listaCompras = []
+import os
+
+lista_compra = []
 
 try:
    
     while True:
         item = input("Digite os itens da sua lista de compras ou deixe em branco para exibir a lista: ")
+        os.system("cls") # Limpa o terminal
         if item != "":
-            listaCompras.append(item)
+            lista_compra.append(item)
+            print(f"{item}, inserido na lista de compras com sucesso!")
             continue
         else:
             break
        
-    listaCompras.sort() # ordenando a lista de nomes em ordem alfabética   
+    lista_compra.sort() # ordenando a lista de nomes em ordem alfabética   
    
 except Exception as e:
     print(f"Erro: {e}")
 
 finally:
-    
-    for n in range(len(listaCompras)):
-        print(f"Item {n+1}: {listaCompras[n]}")
+
+    if len(lista_compra) == 0:
+        print("Lista de compras vazia!")
+    else:
+        print("Lista de compras:\n")
+        for n in range(len(lista_compra)):
+            print(f"Item {n+1}: {lista_compra[n]}")
